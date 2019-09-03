efcee08d730bd7cc3e0056faff55ed460a61c8f5
simoon <420526391@qq.com>
2019-09-04 06:06:38 +0800

    add

---
+++ gulp.js
@@ -0,0 +1,24 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-04 05:46:20
+ * @LastEditTime: 2019-09-04 05:52:42
+ * @LastEditors: Please set LastEditors
+ */
+function js () {
+  console.log('this is js task');
+}
+function css () {
+  console.log('this is css task');
+}
+function watcher () {
+  console.log('this is watcher task');
+}
+function clean () {
+  console.log('this is js task');
+}
+
+
+exports.default = function () {
+  console.log('hello gulp')
+}
